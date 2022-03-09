9806b0577e079a17271ebad26afe8fc99b8c4672
Mary Martinez <mary.diana.martinez@gmail.com>
2022-03-09 15:55:55 -0800

    multiple features working

--- src/components/Editor/Editor.js
+++ src/components/Editor/Editor.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import './Editor.css';
 
-export default function Editor({ title, setTitle }) {
+export default function Editor({ title, setTitle, subtitle, setSubtitle, font, setFont, setAlign }) {
   return (
     <div className="editor">
       <div className="form-control">
@@ -10,11 +10,11 @@ export default function Editor({ title, setTitle }) {
         <label htmlFor="title">Title</label>
       </div>
       <div className="form-control">
-        <input type="text" />
+        <input type="text" value={subtitle} onChange={(e) => setSubtitle(e.target.value)} />
         <label>Subtitle</label>
       </div>
       <div className="form-control">
-        <select>
+        <select onChange={(e) => { setFont(e.target.value); }}>
           <option value="architect">{"Architect's Daughter"}</option>
           <option value="comforter">Comforter</option>
           <option value="fredoka">Fredoka</option>
@@ -28,7 +28,7 @@ export default function Editor({ title, setTitle }) {
       </div>
       <div className="form-control">
         <label>Alignment</label>
-        <div className="radio-group">
+        <div className="radio-group" onChange={(e) => { setAlign(e.target.value); }}>
           <label>
             <input name="align" type="radio" value="left" />
             <i className="ri-align-left"></i>
--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -8,11 +8,14 @@ import './Home.css';
 export default function Home() {
   // add useState calls here for title, subtitle, font, align, and text
   const [title, setTitle] = useState('title');
+  const [subtitle, setSubtitle] = useState('subtitle');
+  const [font, setFont] = useState('architect');
+  const [align, setAlign] = useState('center');
   return (
     <main>
       {/* pass the state variables as props to the presentational components */}
-      <Preview title={title} />
-      <Editor title={title} setTitle={setTitle} />
+      <Preview title={title} subtitle={subtitle} font={font} align={align} />
+      <Editor setTitle={setTitle} setSubtitle={setSubtitle} setFont={setFont} setAlign={setAlign} />
     </main>
   );
 }
